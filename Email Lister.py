71ebae7f375222a8044d0513facb6ce2106f336e
John Rooey <JROOEY@mail.depaul.edu>
2020-05-07 17:10:25 -0400

    workin on it

--- Email Lister.py
+++ Email Lister.py
@@ -1,60 +1,62 @@
-import pandas as pd
+def lister():
 
-raw = pd.read_csv('Florida Test.csv')
-df = pd.DataFrame(raw)
+    import pandas as pd
 
-# Create 'Domain' column
-df['Domain'] = df['Website']
+    raw = pd.read_csv('Florida Test.csv')
+    df = pd.DataFrame(raw)
 
-# Parse Domain from rest of URL in 'Website' column
-for i in range(len(df['Website'])):
-    if 'www.' in df['Website'][i]:
-        split = df['Website'][i].index('www.') + 4
-        df['Domain'][i] = df['Website'][i][split:]
+    # Create 'Domain' column
+    df['Domain'] = df['Website']
 
-    else:
-        split = df['Website'][i].index('//') + 2
-        df['Domain'][i] = df['Website'][i][split:]
+    # Parse Domain from rest of URL in 'Website' column
+    for i in range(len(df['Website'])):
+        if 'www.' in df['Website'][i]:
+            split = df['Website'][i].index('www.') + 4
+            df['Domain'][i] = df['Website'][i][split:]
 
-for i in range(len(df['Domain'])):
-    if df['Domain'][i][-1] == '/':
-        df['Domain'][i] = df['Domain'][i][:-1]
+        else:
+            split = df['Website'][i].index('//') + 2
+            df['Domain'][i] = df['Website'][i][split:]
 
-emails = pd.DataFrame(columns=['Company', 'Name', 'email'])
+    for i in range(len(df['Domain'])):
+        if df['Domain'][i][-1] == '/':
+            df['Domain'][i] = df['Domain'][i][:-1]
 
-#Create list of emails from founders
-space = 0
-for i in range(len(df['Founders'])):
-    rawIndiv = df['Founders'][i].split(', ')
+
+    #Create list of emails from founders
+    space = 0
+    dict = {}
     emlst = []
     companylst = []
     namelst = []
-    for a in rawIndiv:
-        space = a.index(' ')
-        #First@domain
-        emlst.append((a[:space]+'@'+df['Domain'][i]).lower())
-        companylst.append(df['Organization Name'][i])
-        namelst.append(a)
-        #First.Last
-        emlst.append((a[:space]+'.'+a[space+1:]+'@'+df['Domain'][i]).lower())
-        companylst.append(df['Organization Name'][i])
-        namelst.append(a)
-        #FirstLast
-        emlst.append((a[:space]+a[space + 1:]+'@'+df['Domain'][i]).lower())
-        companylst.append(df['Organization Name'][i])
-        namelst.append(a)
-        #First[0]Last
-        emlst.append((a[0]+a[space + 1:]+'@'+df['Domain'][i]).lower())
-        companylst.append(df['Organization Name'][i])
-        namelst.append(a)
-        #First[0].Last
-        emlst.append((a[0] + '.' + a[space + 1:] + '@' + df['Domain'][i]).lower())
-        companylst.append(df['Organization Name'][i])
-        namelst.append(a)
-        dict = {'Company':companylst, 'Name':namelst, 'Email':emlst}
-        temp = pd.DataFrame(dict)
-        emails = pd.concat(temp, emails)
+    for i in range(len(df['Founders'])):
+        rawIndiv = df['Founders'][i].split(', ')
+        for a in rawIndiv:
+            space = a.index(' ')
+            #First@domain
+            emlst.append((a[:space]+'@'+df['Domain'][i]).lower())
+            companylst.append(df['Organization Name'][i])
+            namelst.append(a)
+            #First.Last
+            emlst.append((a[:space]+'.'+a[space+1:]+'@'+df['Domain'][i]).lower())
+            companylst.append(df['Organization Name'][i])
+            namelst.append(a)
+            #FirstLast
+            emlst.append((a[:space]+a[space + 1:]+'@'+df['Domain'][i]).lower())
+            companylst.append(df['Organization Name'][i])
+            namelst.append(a)
+            #First[0]Last
+            emlst.append((a[0]+a[space + 1:]+'@'+df['Domain'][i]).lower())
+            companylst.append(df['Organization Name'][i])
+            namelst.append(a)
+            #First[0].Last
+            emlst.append((a[0] + '.' + a[space + 1:] + '@' + df['Domain'][i]).lower())
+            companylst.append(df['Organization Name'][i])
+            namelst.append(a)
+            dict = {'Company':companylst, 'Name':namelst, 'Email':emlst}
 
+    emails = pd.DataFrame(dict)
 
-#print(emails.head())
-# print(df['Founders'].head())
+    emails.to_csv('TestResults.csv', index=False)
+
+    return()
