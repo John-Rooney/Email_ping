4469cf18bd6c3186f66b589a3f23e35faaeeef8c
John Rooey <JROOEY@mail.depaul.edu>
2020-05-07 16:34:14 -0400

    workin on it

---
+++ Email Lister.py
@@ -0,0 +1,59 @@
+import pandas as pd
+
+raw = pd.read_csv('Florida Test.csv')
+df = pd.DataFrame(raw)
+
+# Create 'Domain' column
+df['Domain'] = df['Website']
+
+# Parse Domain from rest of URL in 'Website' column
+for i in range(len(df['Website'])):
+    if 'www.' in df['Website'][i]:
+        split = df['Website'][i].index('www.') + 4
+        df['Domain'][i] = df['Website'][i][split:]
+
+    else:
+        split = df['Website'][i].index('//') + 2
+        df['Domain'][i] = df['Website'][i][split:]
+
+for i in range(len(df['Domain'])):
+    if df['Domain'][i][-1] == '/':
+        df['Domain'][i] = df['Domain'][i][:-1]
+
+emails = pd.DataFrame(columns=['Company', 'Name', 'email'])
+
+#Create list of emails from founders
+space = 0
+for i in range(len(df['Founders'])):
+    rawIndiv = df['Founders'][i].split(', ')
+    emlst = []
+    companylst = []
+    namelst = []
+    for a in rawIndiv:
+        space = a.index(' ')
+        #First@domain
+        emlst.append((a[:space]+'@'+df['Domain'][i]).lower())
+        companylst.append(df['Organization Name'][i])
+        namelst.append(a)
+        #First.Last
+        emlst.append((a[:space]+'.'+a[space+1:]+'@'+df['Domain'][i]).lower())
+        companylst.append(df['Organization Name'][i])
+        namelst.append(a)
+        #FirstLast
+        emlst.append((a[:space]+a[space + 1:]+'@'+df['Domain'][i]).lower())
+        companylst.append(df['Organization Name'][i])
+        namelst.append(a)
+        #First[0]Last
+        emlst.append((a[0]+a[space + 1:]+'@'+df['Domain'][i]).lower())
+        companylst.append(df['Organization Name'][i])
+        namelst.append(a)
+        #First[0].Last
+        emlst.append((a[0] + '.' + a[space + 1:] + '@' + df['Domain'][i]).lower())
+        companylst.append(df['Organization Name'][i])
+        namelst.append(a)
+        #for w in emlst:
+
+
+
+#print(emails.head())
+# print(df['Founders'].head())
