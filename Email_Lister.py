00e7e9bae872fcc7824f9a68cf0e19fb4eda5ba1
John Rooey <JROOEY@mail.depaul.edu>
2020-05-11 15:10:42 -0400

    hi

--- Email_Lister.py
+++ Email_Lister.py
@@ -1,11 +1,13 @@
-def lister():
+import numpy as np
+import main
+import pandas as pd
 
-    import pandas as pd
-    lst=input('Which List:')
-    raw = pd.read_csv(lst)
+def lister():
+    # lst=input('Which List:')
+    raw = pd.read_csv('Sample15.csv')
     df = pd.DataFrame(raw)
 
-    # Create 'Domain' column
+    # Create 'Domain' and 'Email Valid columns
     df['Domain'] = df['Website']
 
     # Parse Domain from rest of URL in 'Website' column
@@ -22,9 +24,7 @@ def lister():
         if df['Domain'][i][-1] == '/':
             df['Domain'][i] = df['Domain'][i][:-1]
 
-
-    #Create list of emails from founders
-    space = 0
+    # Create list of emails from founders
     dict = {}
     emlst = []
     companylst = []
@@ -33,30 +33,39 @@ def lister():
         rawIndiv = df['Founders'][i].split(', ')
         for a in rawIndiv:
             space = a.index(' ')
-            #First@domain
-            emlst.append((a[:space]+'@'+df['Domain'][i]).lower())
-            companylst.append(df['Organization Name'][i])
-            namelst.append(a)
-            #First.Last
-            emlst.append((a[:space]+'.'+a[space+1:]+'@'+df['Domain'][i]).lower())
-            companylst.append(df['Organization Name'][i])
-            namelst.append(a)
-            #FirstLast
-            emlst.append((a[:space]+a[space + 1:]+'@'+df['Domain'][i]).lower())
-            companylst.append(df['Organization Name'][i])
-            namelst.append(a)
-            #First[0]Last
-            emlst.append((a[0]+a[space + 1:]+'@'+df['Domain'][i]).lower())
-            companylst.append(df['Organization Name'][i])
-            namelst.append(a)
-            #First[0].Last
-            emlst.append((a[0] + '.' + a[space + 1:] + '@' + df['Domain'][i]).lower())
-            companylst.append(df['Organization Name'][i])
-            namelst.append(a)
-            dict = {'Company':companylst, 'Name':namelst, 'Email':emlst}
+            # First@domain
+            em1 = (a[:space] + '@' + df['Domain'][i]).lower()
+            list_build(em1, emlst, companylst, namelst, i, a)
+
+            # First.Last
+            em2 = (a[:space] + '.' + a[space + 1:] + '@' + df['Domain'][i]).lower()
+            list_build(em2, emlst, companylst, namelst, i, a)
+
+            # FirstLast
+            em3 = (a[:space] + a[space + 1:] + '@' + df['Domain'][i]).lower()
+            list_build(em3, emlst, companylst, namelst, i, a)
+
+            # First[0]Last
+            em4 = (a[0] + a[space + 1:] + '@' + df['Domain'][i]).lower()
+            list_build(em4, emlst, companylst, namelst, i, a)
+
+            # First[0].Last
+            em5 = (a[0] + '.' + a[space + 1:] + '@' + df['Domain'][i]).lower()
+            list_build(em5, emlst, companylst, namelst, i, a)
+
+            dict = {'Company': companylst, 'Name': namelst, 'Email': emlst}
 
     emails = pd.DataFrame(dict)
-    #print(emails.head())
-    #emails.to_csv('TestResults.csv', index=False)
-
+    # print(emails.head())
+    # emails.to_csv('TestResults.csv', index=False)
     return emails
+
+
+def list_build(em, emlst, companylst, namelst, i, a):
+    emlst.append(em)
+    companylst.append(df['Organization Name'][i])
+    namelst.append(a)
+    return emlst, companylst, namelst
+
+
+lister()
